88f0ac51dbf35571b5b59bb3f9aebe410176f35b
RomaPatel03 <romapatel1@usf.edu>
2025-12-06 19:30:25 -0500

    Update clean_data.R

--- clean_data.R
+++ clean_data.R
@@ -4,9 +4,9 @@ remove_missing_rows <- function(df) {
 }
 test_df <- data.frame(a = c(1, NA, 3), b = c(NA, 2, 3))
 remove_missing_rows(test_df)
+  a b
+3 3 3
 # remove_missing_rows: Removes rows that contain any missing values
-# Input: a data frame
-# Output: a data frame with no missing values
 remove_missing_rows <- function(df) {
   df[complete.cases(df), ]
 }
